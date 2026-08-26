265259231359b1ad1f2045e678effdeb73554e37
idatumhimanshu-alt <idatum.himanshu@gmail.com>
2026-08-26 10:44:07 +0000

    ...

---
+++ replit.nix
@@ -0,0 +1,32 @@
+{ pkgs }: {
+  deps = [
+    pkgs.glib
+    pkgs.nss
+    pkgs.nspr
+    pkgs.atk
+    pkgs.at-spi2-atk
+    pkgs.at-spi2-core
+    pkgs.cups
+    pkgs.dbus
+    pkgs.expat
+    pkgs.alsa-lib
+    pkgs.cairo
+    pkgs.pango
+    pkgs.gdk-pixbuf
+    pkgs.gtk3
+    pkgs.libxkbcommon
+    pkgs.mesa
+    pkgs.libgbm
+    pkgs.libdrm
+    pkgs.udev
+    pkgs.xorg.libX11
+    pkgs.xorg.libXcomposite
+    pkgs.xorg.libXdamage
+    pkgs.xorg.libXext
+    pkgs.xorg.libXfixes
+    pkgs.xorg.libXrandr
+    pkgs.xorg.libxcb
+    pkgs.xorg.libxshmfence
+    pkgs.xorg.libXScrnSaver
+  ];
+}
